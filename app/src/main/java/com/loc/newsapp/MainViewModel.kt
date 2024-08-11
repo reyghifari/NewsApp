2bb33d938e6645cc088e4a1e7db212ed3290efeb
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2024-08-11 12:31:36 +0700

    [Feat] : handle transparent bar in dark mode

--- app/src/main/java/com/loc/newsapp/MainViewModel.kt
+++ app/src/main/java/com/loc/newsapp/MainViewModel.kt
@@ -1,8 +1,16 @@
 package com.loc.newsapp
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.loc.newsapp.domain.usecase.AppEntryUseCases
+import com.loc.newsapp.view.nvgraph.Route
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +18,22 @@ class MainViewModel @Inject constructor(
     useCases: AppEntryUseCases
 ) : ViewModel() {
 
+     var splashCondition by mutableStateOf(true)
+         private set
+
+    var startDestination by mutableStateOf(Route.AppStartNavigation.route)
+        private set
+
+    init {
+        useCases.readAppEntry().onEach { shouldStartFromHome ->
+            startDestination = if (shouldStartFromHome){
+                Route.NewsNavigation.route
+            }else{
+                Route.AppStartNavigation.route
+            }
+            delay(300)
+            splashCondition = false
+        }.launchIn(viewModelScope)
+    }
 
 }
